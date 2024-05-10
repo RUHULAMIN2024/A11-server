8fe086264565b9b79d48aa2a0cda2c5a306d15d9
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-05-10 15:43:31 +0600

    get single food

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express= require('express')
-const cors= require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 
-const port= process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-const app=express()
+const app = express()
 
 app.use(cors({
-    origin:'*',
-    credentials: true,
-    optionsSuccessStatus:200,
+  origin: '*',
+  credentials: true,
+  optionsSuccessStatus: 200,
 }))
 
 app.use(express.json())
@@ -20,39 +20,47 @@ const uri = `mongodb+srv://${process.env.KEY}:${process.env.KEY}@cluster0.rth5hq
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-
-        const foodsCollection=client.db('flavorFusion').collection('foods')
-
-
-        app.get('/foods', async(req, res)=>{
-
-            const result= await foodsCollection.find().toArray()
-            res.send(result)
-        })
-
-      
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-    
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
+});
+async function run() {
+  try {
+
+    const foodsCollection = client.db('flavorFusion').collection('foods')
+
+
+    app.get('/foods', async (req, res) => {
+
+      const result = await foodsCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/single-food/:id', async (req, res) => {
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result = await foodsCollection.findOne(query)
+      res.send(result)
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 
 
-app.get('/', (req, res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
 app.listen(port)
