3725ec5933160f6933a5f26e0bef760446d32d51
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-05-10 11:05:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express= require('express')
+const cors= require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config()
+
+const port= process.env.PORT || 5000
+
+const app=express()
+
+app.use(cors({
+    origin:'*',
+    credentials: true,
+    optionsSuccessStatus:200,
+}))
+
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.KEY}:${process.env.KEY}@cluster0.rth5hqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+
+        const foodsCollection=client.db('flavorFusion').collection('foods')
+
+
+        app.get('/foods', async(req, res)=>{
+
+            const result= await foodsCollection.find().toArray()
+            res.send(result)
+        })
+
+      
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running')
+})
+
+app.listen(port)
